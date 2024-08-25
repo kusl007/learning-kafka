c5e4d84b75014a753e7d5fa1472a8878c579b455
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-26 00:20:14 +0545

    final commit

--- admin.js
+++ admin.js
@@ -1,1 +1,31 @@
-const {}=require
+const { kafka } = require("./client");
+
+// const { Kafka } = require('kafkajs')
+
+// const kafka = new Kafka({
+//   clientId: 'my-app',
+// })
+
+async function init() {
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Adming Connection Success...");
+
+  console.log("Creating Topic [rider-updates]");
+  await admin.createTopics({
+    topics: [
+      {
+        topic: "rider-updates",
+        numPartitions: 2,
+      },
+    ],
+  });
+  console.log("Topic Created Success [rider-updates]");
+
+  console.log("Disconnecting Admin..");
+  await admin.disconnect();
+}
+
+init();
+// console.log(kafka)
